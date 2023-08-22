ececf9582956df9354c9e91ce65ffa3dc4e9d95d
muthu <marxmuthu2020@gmail.com>
2023-08-22 15:19:04 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-export default function Navbar () {
+export default function Navbar() {
     return (
         <div className="navbar">
-            <a className="navbar-item" href="#gallery">Gallery</a>
-            <a className="navbar-item" href="#flavours">Flavours</a>
-            <a className="navbar-item" href="#shops">Shops</a>
+            <a className="navbar-item" href="#gallery">Courses</a>
+            <a className="navbar-item" href="#flavours">About</a>
+            <a className="navbar-item" href="#shops">Contact US</a>
         </div>
     )
 };
